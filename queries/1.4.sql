081d8187eab4bc12ee99b403d0ac0181a79cdf9b
Iryna <dobrodii2017@gmail.com>
2021-12-11 18:51:41 +0200

    update_queries_1.4,1.8,schema

--- queries/1.4.sql
+++ queries/1.4.sql
@@ -1,5 +1,5 @@
 # 1.4. Вывести все посты со списком тегов, разделенных запятыми.
-select post.id, post.title, GROUP_CONCAT(t.title SEPARATOR ',') from post_tag
+select post.id, post.title, GROUP_CONCAT(t.title SEPARATOR ','), count(t.id) total_tags from post_tag
 inner join post on post_tag.post_id = post.id
 inner join tag t on post_tag.tag_id = t.id
-group by post.id;
+group by post.id having total_tags>=2;
